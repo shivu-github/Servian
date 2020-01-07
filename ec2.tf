3408cefcced84d001b64a8ec4dd7d2a1747082d6
shivu-github <56473817+shivu-github@users.noreply.github.com>
2020-01-07 16:58:17 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "servian-ec2" {
                 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
                 sudo chmod +x /usr/local/bin/docker-compose
                 sudo mkdir /shivu
-                sudo chmod o+x /shivu
+                sudo chmod o+wx /shivu
   EOF
   tags = {
           Name = "servian-ec2"
