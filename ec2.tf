aec68cb866c6b308aee36b87102f11e77a1f7063
shivu-github <56473817+shivu-github@users.noreply.github.com>
2020-01-07 18:21:44 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -11,6 +11,17 @@ resource "aws_instance" "servian-ec2" {
   key_name               = "servian"
   vpc_security_group_ids = ["${aws_security_group.servian-sg.id}"]
   
+  user_data = <<EOF
+                #!/bin/bash
+                sudo yum update -y
+                sudo yum install docker -y 
+                sudo service docker start
+                sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+                sudo chmod +x /usr/local/bin/docker-compose
+                sudo mkdir /shivu
+                sudo chmod o+wx /shivu
+  EOF
+
   tags = {
           Name = "servian-ec2"
          }
